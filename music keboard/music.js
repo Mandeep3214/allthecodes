e7393e891684d0ee2c636753a24fc2ef51db402e
Mandeep <mandeepsharma3214@gmail.com>
2017-06-19 14:48:01 +0530

    Add files via upload

---
+++ music keboard/music.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+
+
+	$("#sa").click(function(){
+
+			var file="e.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#re").click(function(){
+
+			var file="f.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#ga").click(function(){
+
+			var file="g.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#ma").click(function(){
+
+			var file="h.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#pa").click(function(){
+
+			var file="i.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#dha").click(function(){
+
+			var file="j.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#ni").click(function(){
+
+			var file="n.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#saa").click(function(){
+
+			var file="o.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+
+	$("#ree").click(function(){
+
+			var file="p.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#gaa").click(function(){
+
+			var file="t.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+	$("#maa").click(function(){
+
+			var file="r.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+
+	$("#paa").click(function(){
+
+			var file="s.mp3";
+			$("audio").attr("src",file).toggle(trigger("play"));
+
+	});
+
+
+
+});
